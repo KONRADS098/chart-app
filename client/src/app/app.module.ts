a1643955174ed4c349af5124b2fbaaa0130260c7
Konrad98 <konrad-s@outlook.com>
2021-11-24 21:12:14 +0100

    - added navbar - added button styling

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { ChartComponent } from './component/chart/chart.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ChartComponent
+    ChartComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
