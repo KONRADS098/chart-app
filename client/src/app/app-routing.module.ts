cf92004908481791b728d36bfe22a445b7640353
Konrad98 <konrad-s@outlook.com>
2021-11-24 20:47:21 +0100

    - added dependencies, and edited config to support dependencies - created components and styling for components - added navigation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from "./component/home/home.component";
+import { ChartComponent } from "./component/chart/chart.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'welcome', component: HomeComponent },
+  { path: 'chart', component: ChartComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
