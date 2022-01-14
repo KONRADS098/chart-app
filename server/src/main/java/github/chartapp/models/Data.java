42bc2c12930d380b66d0baebc072aa08bae1b425
Konrad <konrad-s@outlook.com>
2022-01-14 21:14:45 +0100

    Expanded back-end to return data based on selected RequestParam

--- server/src/main/java/github/chartapp/models/Data.java
+++ server/src/main/java/github/chartapp/models/Data.java
@@ -1,19 +1,17 @@
 package github.chartapp.models;
 
-import java.sql.Timestamp;
+import java.time.LocalDate;
 
 public class Data {
     private int id;
     private double score;
-    private Timestamp submitDate;
+    private String groupName;
+    private LocalDate submitDate;
 
-    public Data() {
-
-    }
-
-    public Data(int id, double score, Timestamp submitDate) {
+    public Data(int id, double score, String groupName,LocalDate submitDate) {
         this.id = id;
         this.score = score;
+        this.groupName = groupName;
         this.submitDate = submitDate;
     }
 
@@ -33,11 +31,19 @@ public class Data {
         this.score = score;
     }
 
-    public Timestamp getSubmitDate() {
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public LocalDate getSubmitDate() {
         return submitDate;
     }
 
-    public void setSubmitDate(Timestamp submitDate) {
+    public void setSubmitDate(LocalDate submitDate) {
         this.submitDate = submitDate;
     }
 }
--- server/src/main/java/github/chartapp/rest/DataController.java
+++ server/src/main/java/github/chartapp/rest/DataController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -19,9 +20,27 @@ public class DataController {
     @GetMapping()
     public List<Data> getAnswers(@RequestParam(value = "filter1", required = false) String filter1,
                                  @RequestParam(value = "filter1", required = false) String filter2) {
+        if (filter1 != null) {
+            return List.of(
+                    new Data(0, 90, "Class 1",LocalDate.of(2022, 1, 12)),
+                    new Data(0, 90, "Class 2",LocalDate.of(2022, 1, 13)),
+                    new Data(0, 90, "Class 3",LocalDate.of(2022, 1, 15)),
+                    new Data(0, 90, "Class 4",LocalDate.of(2022, 1, 14)),
+                    new Data(0, 90, "Class 5",LocalDate.of(2022, 1, 19))
+                    );
+        }
+
+        if (filter2 != null) {
+            return List.of(
+                    new Data(0, 90, "Group 1",LocalDate.of(2022, 1, 12)),
+                    new Data(0, 90, "Group 2",LocalDate.of(2022, 2, 13)),
+                    new Data(0, 90, "Group 3",LocalDate.of(2022, 3, 14)),
+                    new Data(0, 90, "Group 4",LocalDate.of(2022, 4, 15)),
+                    new Data(0, 90, "Group 5",LocalDate.of(2022, 5, 16))
+                    );
+        }
+
         return null;
     }
-
-
 
 }
